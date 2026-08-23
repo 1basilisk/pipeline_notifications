39088624816cbe085cbe81d182b04758235fce96
1basilisk <1basilisk@github.com>
2026-08-23 23:51:07 +0530

    initial commit

---
+++ src/main/java/com/basilisk/pipeline_notification_service/PipelineNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.basilisk.pipeline_notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineNotificationServiceApplication.class, args);
+	}
+
+}
